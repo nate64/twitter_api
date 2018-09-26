cf8e64ef63c18d3497b799146fd260acdc4a2600
Nate <nathanestes@gmail.com>
2018-09-26 12:12:13 -0600

    just need to generate keys

--- app/controllers/api/tweets_controller.rb
+++ app/controllers/api/tweets_controller.rb
@@ -1,4 +1,12 @@
 class Api::TweetsController < ApplicationController
+  
   def index
+    render json: TwitterClient.timeline
   end
+
+  def search
+    render json: TwitterClient.by_user(params[:term])
+  end
+
+
 end
--- lib/twitter_client.rb
+++ lib/twitter_client.rb
@@ -1,21 +1,36 @@
-class twitterClient
-
+class TwitterClient
   def self.timeline
-    client.home_timeline 
+    client.home_timeline
+  end
+
+  def self.by_user(term)
+    tweets = []
+    client.search("from:#{term}").each do |tweet|
+      user = tweet.user || break
+      tweets << {
+        id: tweet.id,
+        text: tweet.text,
+        user: {
+          screen_name: user.screen_name,
+          name: user.name,
+          url: user.url,
+          profile_image_url: user.profile_image_url.to_s
+        }
+      }
+    end
   end
 
   def self.tweet(message)
     client.update(message)
   end
 
-
-private
-  def self.twitterClient
-    Twitter::Rest::Client.new do |config|
-      config.consumer_key         =['TWITTER_CONSUMER_KEY']
-      config.consumer_secret      =['TWITER_CONSUMER_SECRET']
-      config.access_token         =['TWITTER_ACCESS_TOKEN']
-      config.access_token_secet   =['TWITTER_ACCESS_TOKEN_SECRET']
+  private
+    def self.client
+      Twitter::REST::Client.new do |config|
+        config.consumer_key         = ENV['TWITTER_CONSUMER_KEY']
+        config.consumer_secret      = ENV['TWITTER_CONSUMER_SECRET']
+        config.access_token         = ENV['TWITTER_ACCESS_TOKEN']
+        config.access_token_secret  = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+      end
     end
-  end
 end
